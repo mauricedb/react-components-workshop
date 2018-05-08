51d9404e9f1a06dc2df667aa5efffbc2bf142bcd
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-05-08 13:56:06 +0200

    Use material design light

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="mdl-grid">
+          <header className="mdl-cell mdl-cell--12-col App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+        </div>
+        <div className="mdl-grid">
+          <div className="mdl-cell mdl-cell--8-col mdl-cell--2-offset">
+          </div>
+        </div>
       </div>
     );
   }
