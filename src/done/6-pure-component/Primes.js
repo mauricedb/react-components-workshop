1a3835fa4a380f794035ef778b0df2e148e735dc
Maurice de Beijer <maurice.de.beijer@gmail.com>
2018-05-19 09:48:09 +0200

    Pure component

---
+++ src/done/6-pure-component/Primes.js
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+
+class Primes extends PureComponent {
+  render() {
+    const { primeCount } = this.props;
+    const primes = [];
+
+    console.time('Calculating primes');
+    for (let prime = 2; prime < primeCount; prime++) {
+      let isPrime = true;
+      for (let i = 2; i < prime - 1; i++) {
+        if (prime % i === 0) {
+          isPrime = false;
+        }
+      }
+      if (isPrime) {
+        primes.push(prime);
+      }
+    }
+    console.timeEnd('Calculating primes');
+
+    return (
+      <div>
+        <h3>Primes</h3>
+        {primes.join(', ')}
+      </div>
+    );
+  }
+}
+
+export default Primes;
